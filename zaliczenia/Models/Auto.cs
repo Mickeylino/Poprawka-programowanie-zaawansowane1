944f805d5162b9e124bd623b925a1cfc32c67c4a
Mickeylino <mjagodabusiness@gmail.com>
2024-02-19 12:54:21 +0100

    innit main

---
+++ zaliczenia/Models/Auto.cs
@@ -0,0 +1,15 @@
+﻿namespace zaliczenia.Models
+{
+    public class Auto
+    {
+        public int Id { get; set; }
+        public string Marka { get; set; }
+        public string Nazwa { get; set; }
+        public string Wlasciciel { get; set; }
+
+        public Auto()
+        {
+            
+        }
+    }
+}
